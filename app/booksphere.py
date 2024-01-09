c2a6c6c93e91955d1da61028d5c0f7dee4da7138
Bravin <onsasebravin853@gmail.com>
2024-01-09 19:25:02 +0300

    base in py file

--- app/booksphere.py
+++ app/booksphere.py
@@ -2,3 +2,5 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 import click
 import os
+
+Base = declarative_base()
